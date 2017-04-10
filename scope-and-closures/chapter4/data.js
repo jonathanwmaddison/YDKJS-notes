98828e337236950bb1ef52c7e338390ab9648df8
Jonathan Maddison <jonathanwmaddison@gmail.com>
2017-04-10 12:16:01 -0700

    add notes chapter 4

---
+++ scope-and-closures/chapter4/data.js
@@ -0,0 +1,25 @@
+//CORE_CONCEPTS cover new material that I need to review. Since a lot of the book is review for me I'm cherry picking things that I feel I need more review on.
+/*
+Go to Line 4: data structure (6 lines to copy)
+    {
+        concept: ,
+        definition: ,
+        syntax: null,
+        link: null,
+    },
+*/
+
+const CORE_CONCEPTS = [
+    {
+        concept: "hoisting",
+        definition: "the egg (declaration) comes before the chicken (assignment).",
+        syntax: null,
+        link: null,
+    },
+    {
+        concept: "Hoisting...Again",
+        quote:"We can be tempted to look at var a = 2; as one statement, but the JavaScript Engine does not see it that way. It sees var a and a = 2 as two separate statements, the first one a compiler-phase task, and the second one an execution-phase task." ,
+        syntax: null,
+        link: "https://github.com/getify/You-Dont-Know-JS/blob/master/scope%20%26%20closures/ch4.md#review-tldr",
+    },
+]
